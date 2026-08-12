5b45aeb377300bfcc430fed9771998885aabaa76
thisisommore <ommore501@gmail.com>
2026-08-12 12:22:58 +0530

    Use real Pi Dev app icon as favicon

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,12 +1,12 @@
 <script lang="ts">
 	import './layout.css';
-	import favicon from '$lib/assets/favicon.svg';
 
 	let { children } = $props();
 </script>
 
 <svelte:head>
-	<link rel="icon" href={favicon} />
+	<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+	<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
 	<meta name="theme-color" content="#0b0c10" />
 </svelte:head>
 
